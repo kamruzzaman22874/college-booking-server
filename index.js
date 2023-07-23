424af8211d3a8c8913d31b019d0cb43ed7324734
Md. Jamal Hossen <joybd.eee@gmail.com>
2023-07-23 21:26:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.apl9htr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const collegeCollection = client.db("collegeBooking").collection("college");
+
+    app.get("/college", async(req, res) => {
+         const result = await collegeCollection.find({}).toArray();
+         res.send(result);
+    })
+
+    app.get("/colleges/:id", async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await collegeCollection.findOne(query);
+        res.send(result);
+    })
+    app.get("/admission", async (req , res) =>{
+        const result = await collegeCollection.find({}).toArray();
+        res.send(result);
+    })
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('College booking server is running')
+})
+
+app.listen(port, () => {
+  console.log(`College booking server is running on port ${port}`)
+})
